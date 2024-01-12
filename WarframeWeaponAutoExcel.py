1da17f4f134ad7cba4faf2554823663a76a60a1e
ValentinsKoposovs <valentins.koposovs@edu.rtu.lv>
2024-01-12 20:17:24 +0200

    Updated to work with Rifle type weapons and creates excel file

--- WarframeWeaponAutoExcel.py
+++ WarframeWeaponAutoExcel.py
@@ -23,33 +23,11 @@ if(TheSite.status_code == 200):
     Souped = RawSite.find_all(class_="pi-item pi-item-spacing pi-title pi-secondary-background")
     WeaponName = Souped[0].string
 
+    InfoList = []
+    InfoList.append(WeaponName)
+
     if(WeaponSlot == "Primary" and (WeaponType == "Bow" or WeaponType == "Crossbow" or WeaponType == "Exalted Weapon" or WeaponType == "Launcher" or WeaponType == "Rifle" or WeaponType == "Sniper Rifle" or WeaponType == "Speargun" or WeaponName == "Shedu")):
         
-        Souped = RawSite.find_all('div', attrs = {'data-source' : "Attack1CritChance"})
-        Value = Souped[0].find_all(class_="pi-data-value pi-font")
-        CriticalChance = Value[0].string
-        CriticalChance = CriticalChance[:-1]
-        CriticalChance = float(CriticalChance)
-        
-        Souped = RawSite.find_all('div', attrs = {'data-source' : "Attack1CritMultiplier"})
-        Value = Souped[0].find_all(class_="pi-data-value pi-font")
-        CriticalDamage = Value[0].string
-        CriticalDamage = CriticalDamage[:-1]
-        CriticalDamage = float(CriticalDamage)
-
-        Souped = RawSite.find_all('div', attrs = {'data-source' : "Attack1StatusChance"})
-        Value = Souped[0].find_all(class_="pi-data-value pi-font")
-        StatusChance = Value[0].string
-        StatusChance = StatusChance[:-1]
-        StatusChance = float(StatusChance)
-
-        Souped = RawSite.find_all('div', attrs = {'data-source' : "Attack1FireRate"})
-        Value = Souped[0].find_all(class_="pi-data-value pi-font")
-        FireRate = Value[0].string
-        FireRate = FireRate.split(" ")
-        FireRate = FireRate[0]
-        FireRate = float(FireRate)
-
         Souped = RawSite.find_all('div', attrs = {'data-source' : "Attack1Multishot"})
         Value = Souped[0].find_all(class_="pi-data-value pi-font")
         MSandDMG = Value[0].string
@@ -58,16 +36,40 @@ if(TheSite.status_code == 200):
         Multishot = float(Multishot)
         Damage = MSandDMG[1][1:]
         Damage = float(Damage)
+        InfoList.append(Damage)
+        InfoList.append(Multishot)
 
-        print("Weapon slot:", WeaponSlot)
-        print("Weapon Type:", WeaponType)
-        print("Weapon name:", WeaponName)
-        print("Damage:", Damage)
-        print("Multishot:", Multishot)
-        print("Fire Rate:", FireRate)
-        print("Critical Chance:", CriticalChance)
-        print("Critical Damage:", CriticalDamage)
-        print("Status Chance:", StatusChance)
+        Souped = RawSite.find_all('div', attrs = {'data-source' : "Attack1FireRate"})
+        Value = Souped[0].find_all(class_="pi-data-value pi-font")
+        FireRate = Value[0].string
+        FireRate = FireRate.split(" ")
+        FireRate = FireRate[0]
+        FireRate = float(FireRate)
+        InfoList.append(FireRate)
+
+        Souped = RawSite.find_all('div', attrs = {'data-source' : "Attack1CritChance"})
+        Value = Souped[0].find_all(class_="pi-data-value pi-font")
+        CriticalChance = Value[0].string
+        CriticalChance = CriticalChance[:-1]
+        CriticalChance = float(CriticalChance) * 0.01
+        InfoList.append(CriticalChance)
+        
+        Souped = RawSite.find_all('div', attrs = {'data-source' : "Attack1CritMultiplier"})
+        Value = Souped[0].find_all(class_="pi-data-value pi-font")
+        CriticalDamage = Value[0].string
+        CriticalDamage = CriticalDamage[:-1]
+        CriticalDamage = float(CriticalDamage)
+        InfoList.append(CriticalDamage)
+
+        Souped = RawSite.find_all('div', attrs = {'data-source' : "Attack1StatusChance"})
+        Value = Souped[0].find_all(class_="pi-data-value pi-font")
+        StatusChance = Value[0].string
+        StatusChance = StatusChance[:-1]
+        StatusChance = float(StatusChance) * 0.01
+        InfoList.append(StatusChance)
+    
+        RifleExcel(InfoList)
+
     elif(WeaponSlot == "Primary" and WeaponType == "Shotgun"):
         pass
     elif(WeaponSlot == "Secondary"):
@@ -127,7 +129,4 @@ if(TheSite.status_code == 200):
     else:
         pass
 
-RifleMods0 = [1.55, 0.8, 0.55, 2, 1.2] #dmg,ms,fr,cc,cd
-ShotgunMods0 = [1.65, 1.1, 0.85, 2, 1.1] #dmg,ms,fr,cc,cd
-PistolMods0 = [2.2, 1.1, 0.6, 1.87, 1.1] #dmg,ms,ms/fr,cc,cd
-MeleeMods0 = [1.375, 3, 0.55, 2.75, 0.9] #dmg,range,atkspd,cc,cd
+print("Done!")
